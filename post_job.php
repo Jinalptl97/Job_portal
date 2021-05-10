3d18411f613a77a09a687b3811ce776d2c46c897
himani617 <21434869@student.uwl.ac.uk>
2021-05-10 19:03:39 +0100

    hemani updated 10-05

---
+++ post_job.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Untitled</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,700">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Kaushan+Script">
+    <link rel="stylesheet" href="assets/css/Brands.css">
+    <link rel="stylesheet" href="assets/css/Login-Form-Clean.css">
+    <link rel="stylesheet" href="assets/css/Map-Clean.css">
+    <link rel="stylesheet" href="assets/css/Registration-Form-with-Photo.css">
+    <link rel="stylesheet" href="assets/css/Social-Icons.css">
+    <link rel="stylesheet" href="assets/css/Testimonials.css">
+</head>
+
+<body>
+    <section class="register-photo" style="height:70%">
+        <div class="form-container" >
+            <div class="image-holder"></div>
+            <form method="post">
+                <h2 class="text-center"><strong>Upload</strong>Job</h2>
+                <div class="form-group"><input class="form-control" type="text" name="job" placeholder="Job Title"></div>
+				 <div class="form-group"><input class="form-control" type="text" name="job description" placeholder="Job description"></div>
+				  <div class="form-group"><input class="form-control" type="number" name="vacancy" placeholder="Vacancy number"></div>
+                <div class="form-group"><input class="form-control" type="number" name="basicpay" placeholder="Basic Pay"></div>
+				 <div class="form-group"><input class="form-control" type="text" name="location" placeholder="Location"></div>
+				 <div class="form-group"><input class="form-control" type="text" name="Address" placeholder="Address"></div>
+				 <div class="form-group"><input class="form-control" type="text" name="Postcode" placeholder="Postcode"></div>
+				 
+				 
+	
+	
+	
+ 
+  
+</div>
+<br><br>
+<div class="form-group"><input class="form-control" type="text" name="Phonenumber" placeholder="phone number"></div>
+
+				 
+                <div class="form-group"><button class="btn btn-primary btn-block" type="submit">Post Job</button></div>
+            </form>
+        </div>
+		</div>
+    </section>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.0/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
+    <script src="assets/js/agency.js"></script>
+</body>
+
+</html>
